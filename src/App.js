9492f21dede0ca30788df297918640f027573b1f
stacksonstack <staceycarrillo44@gmail.com>
2020-11-17 14:52:59 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import {yes, no} from './objects';
+
+
 function App() {
-  return <h1>You Can Do This!</h1>;
+  const [toggle, setToggle] = useState(false)
+  return  ( 
+    <>
+    {toggle ? <h1>{yes["yes-statement"]}</h1> : <h1> {no["no-statement"]}</h1> }
+    <img alt="no" onClick ={() => setToggle(!toggle)} src={toggle ? yes["yes-image"] : no['no-image']} />
+    </>
+    )
 }
 
 export default App;
